2e914e35ba38934a20ae55dd385d1a8d9dfe0064
Niketha-S <84368079+Niketha-S@users.noreply.github.com>
2025-04-11 11:00:44 -0500

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tweet-sentiment-stock-impact/', // 👈 Add this line
   plugins: [svelte()],
 })
